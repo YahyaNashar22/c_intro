9c58c2abe92fc92f72db0cce7ebd5306f9480ebf
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-03-29 15:04:01 +0300

    shopping cart test console app

--- main.c
+++ main.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
 #include <string.h>
 
+int intro();
+int shopping_cart();
+int random();
+
 int main()
+{
+   random();
+
+    return 0;
+}
+
+int random()
+{
+    int number = 0;
+    int *p = &number;
+    
+    number = 12;
+    printf("%d\n", number);
+
+    *&number = 40;
+    printf("%d\n", number);
+
+    printf("%d\n", &number);
+    printf("%d\n", *p);
+} 
+
+int shopping_cart()
+{
+    char item[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
+
+    printf("What item would you like to buy?\n");
+    fgets(item, sizeof(item), stdin);
+
+    printf("What is the price for each?\n");
+    scanf("%f", &price);
+
+    printf("How many would you like?\n");
+    scanf("%d", &quantity);
+
+    total = price * quantity;
+
+    printf("%f", total);
+
+    return 0;
+}
+
+int intro()
 {
     int age = 0;
     float gpa = 0.0f;
@@ -26,5 +76,5 @@ int main()
     printf("Your gpa is: %.2f\n", gpa);
     printf("Your grade is: %c\n", grade);
     printf("Your name is: %s\n", name);
-    return 0;
+ 
 }
